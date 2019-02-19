9f0319ef9055d36f2e15f29871808fba780d2113
robbor_io <therealrandomdudeyt@gmail.com>
2019-02-18 20:11:09 -0500

    Fixed easing curves

--- src/other/easing.py
+++ src/other/easing.py
@@ -10,18 +10,23 @@ class Easer():
 	def easeInCubic(t):
 		return t*t*t
 	def easeOutCubic(t):
-		return (--t)*t*t+1
+		q = t - 1
+		return (q)*t*t+1
 	def easeInOutCubic(t):
 		return 4*t*t*t if t<.5 else (t-1)*(2*t-2)*(2*t-2)+1
 	def easeInQuart(t):
 		return t*t*t*t
 	def easeOutQuart(t):
-		return 1-(--t)*t*t*t
+		q = t - 1
+		return 1-(q)*t*t*t
 	def easeInOutQuart(t):
-		return 8*t*t*t*t if t<.5 else 1-8*(--t)*t*t*t
+		q = t - 1
+		return 8*t*t*t*t if t<.5 else 1-8*(q)*t*t*t
 	def easeInQuint():
 		return t*t*t*t*t
 	def easeOutQuint(t):
-		return 1+(--t)*t*t*t*t
+		q = t - 1
+		return 1+(q)*t*t*t*t
 	def easeInOutQuint(t):
-		return 16*t*t*t*t*t if t<.5 else 1+16*(--t)*t*t*t*t
+		q = t - 1
+		return 16*t*t*t*t*t if t<.5 else 1+16*(q)*t*t*t*t
